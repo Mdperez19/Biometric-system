07d49a200167bd70bcdbf155f2c2033d86fb09e5
Mdperez19 <mdperezpriego@gmail.com>
2022-04-04 23:40:23 -0500

    script con Vault2

--- test/script.sh
+++ test/script.sh
@@ -10,4 +10,4 @@ cd ../../test
 
 gcc main.c "../kyber/ref/libpqcrystals_fips202_ref.so" "../kyber/ref/libpqcrystals_kyber1024_ref.so" "../kyber/ref/randombytes.c" "../AES256/opensslaes.c" -lcrypto -lssl
 
-./a.out  ../Biometric-System/resultados/Vault.txt
+./a.out  ../Biometric-System/resultados/Vault2.txt
